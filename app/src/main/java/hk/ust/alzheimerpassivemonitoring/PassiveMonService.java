eb84506b88b34650c5f09a6da483f3a551ca8537
Henry Chan <henrychan699966@gmail.com>
2017-04-20 01:36:16 +0800

    able to start foreground service

--- app/src/main/java/hk/ust/alzheimerpassivemonitoring/PassiveMonService.java
+++ app/src/main/java/hk/ust/alzheimerpassivemonitoring/PassiveMonService.java
@@ -1,11 +1,13 @@
 package hk.ust.alzheimerpassivemonitoring;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.annotation.IntDef;
+
 import android.support.annotation.Nullable;
 import android.widget.Toast;
+
 
 /**
  * Created by henry on 2017-04-18.
@@ -13,14 +15,24 @@ import android.widget.Toast;
 
 public class PassiveMonService extends Service {
 
+    private Notification notification;
+    private int ONGOING_NOTIFICATION_ID;
 
     @Override
     public void onCreate() {
+        notification = new Notification.Builder(getApplicationContext())
+                .setContentTitle("Passive Monitoring")
+                .setContentText("In Progress")
+                .setSmallIcon(R.drawable.notification_icon)
+                .build();
+        ONGOING_NOTIFICATION_ID = (int)System.currentTimeMillis();
         super.onCreate();
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        startForeground(ONGOING_NOTIFICATION_ID,notification);
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
         return super.onStartCommand(intent, flags, startId);
     }
