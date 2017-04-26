a18f8dd6987197b3f0cb99d2f1116e7566a12f3b
cwko <kfoinz@gmail.com>
2017-04-27 00:26:10 +0800

    all graph sample

--- app/src/main/java/hk/ust/alzheimerpassivemonitoring/GraphPlotter.java
+++ app/src/main/java/hk/ust/alzheimerpassivemonitoring/GraphPlotter.java
@@ -19,20 +19,27 @@ import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class GraphPlotter extends AppCompatActivity {
 
@@ -128,13 +135,45 @@ public class GraphPlotter extends AppCompatActivity {
                     PieChart c2 = (PieChart) rootView.findViewById(R.id.chart2);
                     c2.setVisibility(View.GONE);
                     LineChart chart2 = (LineChart) rootView.findViewById(R.id.chart1);
-                    chart2.setData(generateData());
+                    chart2.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+                        private SimpleDateFormat mFormat = new SimpleDateFormat("dd-MM");
+                        @Override
+                        public String getFormattedValue(float value, AxisBase axis) {
+
+                            long millis = TimeUnit.DAYS.toMillis((long) value);
+                            return mFormat.format(new Date(millis));
+                        }
+                    });
+                    chart2.setData(generateStepDistanceData());
                     break;
                 case SLEEP_WAKE_GRAPH:
                     PieChart c3 = (PieChart) rootView.findViewById(R.id.chart2);
                     c3.setVisibility(View.GONE);
                     LineChart chart3 = (LineChart) rootView.findViewById(R.id.chart1);
-                    chart3.setData(generateData());
+                    chart3.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+                        private SimpleDateFormat mFormat = new SimpleDateFormat("HH:mm");
+                        @Override
+                        public String getFormattedValue(float value, AxisBase axis) {
+
+                            long millis = TimeUnit.HOURS.toMillis((long) value);
+                            return mFormat.format(new Date(millis));
+                        }
+                    });
+                    LimitLine ll1 = new LimitLine(300f, "Awake");
+                    ll1.setLineWidth(4f);
+                    ll1.enableDashedLine(10f, 10f, 0f);
+                    ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+                    ll1.setTextSize(10f);
+
+                    LimitLine ll2 = new LimitLine(100f, "Asleep");
+                    ll2.setLineWidth(4f);
+                    ll2.enableDashedLine(10f, 10f, 0f);
+                    ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+                    ll2.setTextSize(10f);
+
+                    chart3.getAxisLeft().addLimitLine(ll1);
+                    chart3.getAxisLeft().addLimitLine(ll2);
+                    chart3.setData(generateSleepWakeData());
                     break;
 //                case LOCATION_RECORD_GRAPH:
 //                    CombinedChart chart3 = (CombinedChart) rootView.findViewById(R.id.chart);
@@ -149,17 +188,62 @@ public class GraphPlotter extends AppCompatActivity {
         }
     }
 
-    static LineData generateData() {
+    static LineData generateStepDistanceData() {
 
         ArrayList<Entry> values = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            float val = (float) (Math.random() * 3) + 3;
-            values.add(new Entry(i, val));
+        long now = TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis());
+        Log.e("now", now+"");
+
+        for (float x = now-7; x < now; x++) {
+            values.add(new Entry(x, x/2 + (float) Math.pow(-1,x) * (x%4)));
         }
-        LineDataSet set = new LineDataSet(values, "DataSet ");
-        set.setDrawIcons(false);
-        return new LineData(set);
+
+        LineDataSet set1 = new LineDataSet(values, "DataSet1 ");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setValueTextColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(1.5f);
+        set1.setDrawCircles(false);
+        set1.setDrawValues(false);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setDrawCircleHole(false);
+
+        LineData data = new LineData(set1);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(9f);
+
+        return data;
+    }
+
+    static LineData generateSleepWakeData() {
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (float x = -8; x < 0; x++) {
+            values.add(new Entry(x, x*(-10)+x%3));
+        }
+        for (float x = 0; x < 16; x++) {
+            values.add(new Entry(x, 400+ (float) Math.pow(-1,x) *(x+x%4)));
+        }
+
+        LineDataSet set1 = new LineDataSet(values, "DataSet2 ");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setValueTextColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(1.5f);
+        set1.setDrawCircles(false);
+        set1.setDrawValues(false);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setDrawCircleHole(false);
+
+        LineData data = new LineData(set1);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(9f);
+
+        return data;
     }
 
     static PieData generatePhoneUsageData(PhoneUsage[] p) {
